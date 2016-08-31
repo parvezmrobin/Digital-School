891e9bd02b9d87bfe48f9fca08e14d31df6bda25
Parvez M Robin <parvezmrobin@outlook.com>
2016-09-01 00:42:51 +0600

    Added primary files

---
+++ Digital School/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Digital_School.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Digital School/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Digital_School.Global" Language="C#" %>
